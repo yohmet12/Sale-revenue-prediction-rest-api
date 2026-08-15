cb0b01de6134c38d4a58b0e972178cf7e06c9cef
yohmet12 <yohannes.endaznaw@gmail.com>
2026-08-15 13:12:25 -0400

    Add backend/app.py

---
+++ backend/app.py
@@ -0,0 +1,74 @@
+
+# Import necessary libraries
+import numpy as np
+import joblib  # For loading the serialized model
+import pandas as pd  # For data manipulation
+from flask import Flask, request, jsonify  # For creating the Flask API
+
+# Initialize Flask app with a name
+superkart_api = Flask("SuperKart Sale Revenue Predictor")
+
+# Load the trained Sale Revenue prediction model
+model = joblib.load("sale_revenue_prediction_model_v1_0.joblib")
+# Define a route for the home page
+@superkart_api.get('/')
+def home():
+    return "Welcome to the SuperKart sale Revenue Prediction API!"
+# Define an endpoint to predict Sale Revenue
+@superkart_api.post('/v1/predict')
+def predict_sales():
+    # Get JSON data from the request
+    data = request.get_json()
+
+    # Extract relevant customer features from the input data. The order of the column names matters.
+    sample = {
+        'Product_Weight': data['Product_Weight'],
+        'Product_Sugar_Content': data['Product_Sugar_Content'],
+        'Product_Allocated_Area': data['Product_Allocated_Area'],
+        'Product_MRP': data['Product_MRP'],
+        'Store_Size': data['Store_Size'],
+        'Store_Location_City_Type': data['Store_Location_City_Type'],
+        'Store_Type': data['Store_Type'],
+        'Product_Id_char': data['Product_Id_char'],
+        'Store_Age_Years': data['Store_Age_Years'],
+        'Product_Type_Category': data['Product_Type_Category']
+    }
+
+    # Convert the extracted data into a DataFrame
+    input_data = pd.DataFrame([sample])
+
+    # Make a churn prediction using the trained model
+    prediction = model.predict(input_data).tolist()[0]
+
+    # Return the prediction as a JSON response
+    return jsonify({'Sales': prediction})
+# Define an endpoint for batch prediction (POST request)
+@superkart_api.post('/v1/predictbatch')
+def predict_rental_price_batch():
+    """
+    This function handles POST requests to the '/v1/predictbatch' endpoint.
+    It expects a CSV file containing property details for multiple properties
+    and returns the predicted rental prices as a dictionary in the JSON response.
+    """
+    # Get the uploaded CSV file from the request
+    file = request.files['file']
+
+    # Read the CSV file into a Pandas DataFrame
+    input_data = pd.read_csv(file)
+
+    # Make predictions for all properties in the DataFrame (get log_prices)
+    predicted_log_prices = model.predict(input_data).tolist()
+
+    # Calculate actual prices
+    predicted_prices = [round(float(np.exp(log_price)), 2) for log_price in predicted_log_prices]
+
+    # Create a dictionary of predictions with property IDs as keys
+    property_ids = input_data['id'].tolist()  # Assuming 'id' is the property ID column
+    output_dict = dict(zip(property_ids, predicted_prices))  # Use actual prices
+
+    # Return the predictions dictionary as a JSON response
+    return output_dict
+
+# Run the Flask app in debug mode
+if __name__ == '__main__':
+    superkart_api.run(debug=True)
